3e04e36e08a0b334a5b259b5a924de2254af00eb
Jansen Vitor <jansen.vitor@sebraemg.com.br>
2015-05-06 14:16:52 -0300

    Add endereço seeder

--- src/database/seeds/InitSeeder.php
+++ src/database/seeds/InitSeeder.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Laraerp\Cidade;
 use Laraerp\Cliente;
+use Laraerp\Endereco;
 use Laraerp\Pessoa;
 
 class InitSeeder extends Seeder {
@@ -21,7 +23,9 @@ class InitSeeder extends Seeder {
         /*
          * Criando Clientes
          */
-        Cliente::create(['pessoa_id' => Pessoa::create(['nome' => 'Teste 01', 'documento' => '94177648531'])->id]);
+        $pessoa01 = Pessoa::create(['nome' => 'Teste 01', 'documento' => '94177648531']);
+        Cliente::create(['pessoa_id' => $pessoa01->id]);
+
         Cliente::create(['pessoa_id' => Pessoa::create(['nome' => 'Teste 02', 'documento' => '78564045230'])->id]);
         Cliente::create(['pessoa_id' => Pessoa::create(['nome' => 'Teste 03', 'documento' => '93913064621'])->id]);
         Cliente::create(['pessoa_id' => Pessoa::create(['nome' => 'Teste 04', 'documento' => '65868138597'])->id]);
@@ -31,6 +35,20 @@ class InitSeeder extends Seeder {
         Cliente::create(['pessoa_id' => Pessoa::create(['nome' => 'Teste 08', 'razao_apelido' => 'Teste Zero Oito LTDA', 'documento' => '42156536000140'])->id]);
         Cliente::create(['pessoa_id' => Pessoa::create(['nome' => 'Teste 09', 'razao_apelido' => 'Teste Zero Nove LTDA', 'documento' => '33474718000179'])->id]);
         Cliente::create(['pessoa_id' => Pessoa::create(['nome' => 'Teste 10', 'razao_apelido' => 'Teste Zero Dez LTDA',  'documento' => '43759234000120'])->id]);
+
+        /*
+         * Criando um endereço para a Pessoa01
+         */
+        $cidade = Cidade::where('nome', 'BELO HORIZONTE')->first();
+
+        Endereco::create([
+            'pessoa_id' => $pessoa01->id,
+            'cep' => '31030080',
+            'logradouro' => 'Rua Alabastro',
+            'numero' => 195,
+            'bairro' => 'Sagrada Familia',
+            'cidade_id' => $cidade->id
+        ]);
     }
 
 }
